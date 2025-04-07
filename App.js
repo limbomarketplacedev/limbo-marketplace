7953e2688fe42669210335c5da53a113a2af218f
limbomarketplacedev <lucassurfcity@gmail.com>
2025-04-07 15:15:59 -0700

    Remove onboarding screen and slider assets

--- App.js
+++ App.js
@@ -1,50 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 import AppStack from './navigation/AppStack';
 import AuthStack from './navigation/AuthStack';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import OnboardingScreen from './screens/Onboarding/OnboardingScreen';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import SplashAnimation from './components/SplashAnimation';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['AsyncStorage has been extracted']);
 
 export default function App() {
   const [user, setUser] = useState(null);
-  const [showOnboarding, setShowOnboarding] = useState(false);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const checkLaunch = async () => {
-      const hasLaunched = await AsyncStorage.getItem('hasLaunched');
-      if (!hasLaunched) {
-        setShowOnboarding(true);
-        await AsyncStorage.setItem('hasLaunched', 'true');
-      }
-    };
-
-    const auth = getAuth();
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
-      setLoading(false);
     });
-
-    checkLaunch();
-    return () => unsubscribe();
+    return unsubscribe;
   }, []);
 
-  if (loading) return <SplashAnimation />;
-
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        {showOnboarding ? (
-          <OnboardingScreen />
-        ) : user ? (
-          <AppStack />
-        ) : (
-          <AuthStack />
-        )}
-      </NavigationContainer>
-    </GestureHandlerRootView>
+    <NavigationContainer>
+      {user ? <AppStack /> : <AuthStack />}
+    </NavigationContainer>
   );
 }
--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -2,20 +2,10 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainTabs from './MainTabs';
 import ListingDetailScreen from '../screens/Listing/ListingDetailScreen';
-import SellerProfileScreen from '../screens/Profile/SellerProfileScreen';
-import SubmitReviewScreen from '../screens/Reviews/SubmitReviewScreen';
-import SubmitBuyerReviewScreen from '../screens/Reviews/SubmitBuyerReviewScreen';
-import BuyerProfileScreen from '../screens/Profile/BuyerProfileScreen';
-import SubscriptionStatusScreen from '../screens/Profile/SubscriptionStatusScreen';
-import LimboStoreScreen from '../screens/Store/LimboStoreScreen';
-import FavoritesScreen from '../screens/Profile/FavoritesScreen';
-import SuccessScreen from '../screens/Post/SuccessScreen';
-import InboxScreen from '../screens/Inbox/InboxScreen';
 import ChatScreen from '../screens/Inbox/ChatScreen';
-import AdminReportsScreen from '../screens/Admin/AdminReportsScreen';
-import UploadBannerScreen from '../screens/Admin/UploadBannerScreen';
 import SupportScreen from '../screens/Support/SupportScreen';
-import SupportInboxScreen from '../screens/Admin/SupportInboxScreen';
+import SubmitReviewScreen from '../screens/Review/SubmitReviewScreen';
+import SubmitBuyerReviewScreen from '../screens/Review/SubmitBuyerReviewScreen';
 
 const Stack = createStackNavigator();
 
@@ -24,21 +14,10 @@ export default function AppStack() {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Main" component={MainTabs} />
       <Stack.Screen name="ListingDetail" component={ListingDetailScreen} />
-      <Stack.Screen name="SellerProfile" component={SellerProfileScreen} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
+      <Stack.Screen name="Support" component={SupportScreen} />
       <Stack.Screen name="SubmitReview" component={SubmitReviewScreen} />
       <Stack.Screen name="SubmitBuyerReview" component={SubmitBuyerReviewScreen} />
-      <Stack.Screen name="BuyerProfile" component={BuyerProfileScreen} />
-      <Stack.Screen name="SubscriptionStatus" component={SubscriptionStatusScreen} />
-      <Stack.Screen name="LimboStore" component={LimboStoreScreen} />
-      <Stack.Screen name="Favorites" component={FavoritesScreen} />
-      <Stack.Screen name="Success" component={SuccessScreen} />
-      <Stack.Screen name="Inbox" component={InboxScreen} />
-      <Stack.Screen name="Chat" component={ChatScreen} />
-      <Stack.Screen name="AdminReports" component={AdminReportsScreen} />
-      <Stack.Screen name="UploadBanner" component={UploadBannerScreen} />
-      <Stack.Screen name="Support" component={SupportScreen} />
-      <Stack.Screen name="SupportInbox" component={SupportInboxScreen} />
-
     </Stack.Navigator>
   );
 }
